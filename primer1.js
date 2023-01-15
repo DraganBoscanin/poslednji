b72636b72468f73664b2be00768f667ebc483b60
Dragan Boscanin <dboscanin@gmail.com>
2023-01-15 01:08:21 +0100

    poslednji pokusaj

---
+++ primer1.js
@@ -0,0 +1,15 @@
+const buttonDugme = document.getElementById("dugme")
+
+
+function promenaPozadine() {
+    const slika = document.body.innerHTML =" SA KOSOVA SI PARE UZEO CETRI FIRME SI OTVORIO  NA VLASTI OSTAJES SVE POTPLACUJES U ZATVOR IDES BRZO VIDECES NA NA NA NA NAAAAA PUSI KURAC PICKO CORAVA"
+    slika = document.body.style.backgroundColor = "grey"
+    
+  
+    
+    
+   
+   
+}
+
+buttonDugme.onclick = promenaPozadine
